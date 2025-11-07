bd198d5f5cd681e3bb8e05ea17b60b0a0f1d6786
matviidev <matvii.shpyruk.dev@gmail.com>
2025-11-07 14:42:19 +0200

    feat: add basic routes

---
+++ src/app.js
@@ -0,0 +1,49 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
+
+var indexRouter = require('./routes/index');
+const movieRouter = require('./routes/movie');
+const searchRouter = require('./routes/search');
+
+var app = express();
+app.use(helmet());
+
+app.use((req, res, next) => {
+  if (req.query.api_key != 123456789) {
+    res.status(401);
+    res.json('Invalid API Key');
+  } else {
+    next();
+  }
+});
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/movie', movieRouter);
+app.use('/search', searchRouter);
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
